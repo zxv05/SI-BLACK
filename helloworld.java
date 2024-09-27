e1a4358c0e9a28370ba08653e87e2f2fc400c5ed
zxv05 <fadhlurrhmanathallah@gmail.com>
2024-09-27 09:53:47 +0700

    Tambah 1

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,7 @@
 public class helloworld{
     public  static void main(String[] args) {
         System.out.println ("Hello, World!"); 
+        System.out.print ("Nama Saya ");
+        System.out.println ("Athallah");
     }
 }
